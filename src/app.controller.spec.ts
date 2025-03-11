a04758429b9e1594c205363b7b0101311738cded
Chaparron <chaparroncoder@gmail.com>
2025-03-11 18:40:21 +0100

    refactor AppController tests to use health check endpoint test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import * as request from 'supertest';
+import { INestApplication } from '@nestjs/common';
 
 describe('AppController', () => {
-  let appController: AppController;
+  let app: INestApplication;
+  let controller: AppController;
 
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
     }).compile();
 
-    appController = app.get<AppController>(AppController);
+    app = module.createNestApplication();
+    await app.init();
+    controller = module.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('/healthcheck (GET)', () => {
+    it('should return health check status', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/healthcheck')
+        .expect(200);
+
+      expect(response.body).toMatchObject({
+        status: 'ok',
+        environment: process.env.NODE_ENV,
+      });
     });
+
   });
 });
