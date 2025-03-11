3fe26334781a5ff2fe02cdfc267bc0370eb0257a
Chaparron <chaparroncoder@gmail.com>
2025-03-11 18:07:26 +0100

    refactor entity names to specify table names for Event and User entities

--- src/domain/entities/event.entity.ts
+++ src/domain/entities/event.entity.ts
@@ -2,7 +2,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 type Duration = { hours: number; minutes: number };
 
-@Entity()
+@Entity({ name: 'events' })
 export class Event {
   @PrimaryGeneratedColumn()
   id: number;
